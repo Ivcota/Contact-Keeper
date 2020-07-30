685bd6a871dd8ca0f366d9f5d49e5ea7535571d9
Ivcota <ivcotad@gmail.com>
2020-07-30 00:22:42 -0700

    base

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,27 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
-// @access  Public 
-router.post('/', (req, res) => {
-    res.send('Register a User')
-});
+// @access  Public
+router.post(
+  "/",
+  [
+      check("name", "Name is required").not().isEmpty(),
+      check('email', 'Please include a valid email').isEmail(),
+      check('password', 'Please enter a passwor dwith 6 or more characters').isLength({min: 6}),
+    ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmprty()) {
+        return res.status(400).json({ errors: error.array() })
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
